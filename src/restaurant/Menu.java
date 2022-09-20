c68c4ccd5c454757622d537a40766c1972d4b71b
Stealthchair <31829676+Stealthchair@users.noreply.github.com>
2022-09-19 20:59:28 -0500

    Fleshed out Menu class

--- src/restaurant/Menu.java
+++ src/restaurant/Menu.java
@@ -5,4 +5,35 @@ import java.util.Date;
 public class Menu {
     private Date lastUpdated;
     private ArrayList<MenuItem> fullMenu;
+
+    public Menu(){
+        this.lastUpdated = new Date();
+    }
+
+    //Overloaded constructor for pre made menu lists
+    public Menu(ArrayList<MenuItem> newMenuList){
+        this.lastUpdated = new Date();
+        this.fullMenu = newMenuList;
+    }
+
+
+    public Date getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Date lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    public ArrayList<MenuItem> getFullMenu() {
+        return fullMenu;
+    }
+
+    public void setFullMenu(ArrayList<MenuItem> fullMenu) {
+        this.fullMenu = fullMenu;
+    }
+
+    //Remove item
+
+    //Add item
 }
--- src/restaurant/main.java
+++ src/restaurant/main.java
@@ -1,9 +1,20 @@
 package restaurant;
 
+import java.util.ArrayList;
+
 public class main {
     public static void main(String[] args){
          MenuItem grapes = new MenuItem(25.00, "Quacks like a duck", Category.APPETIZER);
 
          System.out.println(grapes.getCategory());
+
+         Menu testMenu = new Menu();
+
+
+         ArrayList<MenuItem> testItems = new ArrayList<MenuItem>();
+         testItems.add(grapes);
+        Menu testMenu2 = new Menu(testItems);
+         System.out.println(testMenu.getLastUpdated());
+        System.out.println(testMenu2.getLastUpdated());
     }
 }
