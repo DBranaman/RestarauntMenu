1b0da61d3607e98fc9f731125346b538edf3cc06
Stealthchair <31829676+Stealthchair@users.noreply.github.com>
2022-09-26 20:54:05 -0500

    Bonus complete

--- src/restaurant/Menu.java
+++ src/restaurant/Menu.java
@@ -9,17 +9,16 @@ public class Menu {
     private Date lastUpdated;
     private ArrayList<MenuItem> fullMenu;
 
-    public Menu(){
+    public Menu() {
         this.lastUpdated = new Date();
         this.fullMenu = new ArrayList<MenuItem>();
     }
 
     //Overloaded constructor for pre made menu lists
-    public Menu(ArrayList<MenuItem> newMenuList){
+    public Menu(ArrayList<MenuItem> newMenuList) {
         this.lastUpdated = new Date();
         this.fullMenu = newMenuList;
     }
-
 
 
     public Date getLastUpdated() {
@@ -33,11 +32,12 @@ public class Menu {
     public ArrayList<MenuItem> getFullMenu() {
         return fullMenu;
     }
-    public MenuItem getMenuItem(int index){
+
+    public MenuItem getMenuItem(int index) {
         return fullMenu.get(index);
     }
 
-    public MenuItem getMenuItem(MenuItem item){
+    public MenuItem getMenuItem(MenuItem item) {
         int index = fullMenu.indexOf(item);
         return fullMenu.get(index);
     }
@@ -46,37 +46,70 @@ public class Menu {
         this.fullMenu = fullMenu;
     }
 
-    public void addMenuItem(MenuItem toAdd){
+    public void addMenuItem(MenuItem toAdd) {
+        Boolean duplicate = false;
+        for (MenuItem i : this.getFullMenu()) {
+            if (equals(toAdd, i) && (this.getFullMenu().indexOf(toAdd) != this.getFullMenu().indexOf(i))) {
+                duplicate = true;
+                System.out.println(toAdd.getName() + " is a duplicate of of an existing menu item " + toAdd.getName());
+                return;
+            }
+        }
         fullMenu.add(toAdd);
     }
 
-    public void removeMenuItem(MenuItem toRemove){
+    public void removeMenuItem(MenuItem toRemove) {
         int position = this.fullMenu.indexOf(toRemove);
         this.fullMenu.remove(position);
     }
 
-    public void printFullMenu(){
+    public void printFullMenu() {
         //Sort Menu
         ArrayList<MenuItem> sortedMenu = new ArrayList<>(this.getFullMenu());
         Collections.sort(sortedMenu, new MenuItemComparator());
 
-        for(MenuItem i : sortedMenu ){
+        for (MenuItem i : sortedMenu) {
             printMenuItem(i);
             System.out.println();
         }
     }
 
-    public void printMenuItem(MenuItem item){
+    public void printMenuItem(MenuItem item) {
         item.printInformation();
     }
 
-    public int compareMenuItems(MenuItem item1, MenuItem item2){
+    public int compareMenuItems(MenuItem item1, MenuItem item2) {
         MenuItemComparator comp = new MenuItemComparator();
         int result = comp.compare(item1, item2);
 
         return result;
     }
 
+    public boolean equals(MenuItem item1, MenuItem item2){
+        boolean name = false;
+        boolean price = false;
+        boolean description = false;
+        boolean category = false;
+        if(item1.getName() == item2.getName()){
+            name =  true;
+        }
 
+        if(item1.getPrice() == item2.getPrice()){
+            price = true;
+        }
+
+        if(item1.getDescription() == item2.getDescription()){
+            description = true;
+        }
+
+        if(item1.getCategory() == item2.getCategory()){
+            category = true;
+        }
+        if(name && price && description && category){
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 
--- src/restaurant/main.java
+++ src/restaurant/main.java
@@ -6,15 +6,17 @@ import java.util.Date;
 public class main {
     public static void main(String[] args) {
         MenuItem grapes = new MenuItem("Grapes", 25.00, "Quacks like a duck", Category.APPETIZER);
-        MenuItem cassadilla = new MenuItem("Quesadilla", 3.00, "Dang cassadilla", Category.MAINCOURSE);
+        MenuItem cassadilla = new MenuItem("Quesadilla", 25.00, "Dang cassadilla", Category.MAINCOURSE);
         MenuItem apple = new MenuItem("Apple", 0.25, "Granny's Finest", Category.DESSERT);
         MenuItem yogurt = new MenuItem("Yogurt", 3.47, "Full of bacteria", Category.DESSERT);
+        MenuItem apple2 = new MenuItem("Apple", 0.25, "Granny's Finest", Category.DESSERT);
 
         Menu testMenu = new Menu();
         testMenu.addMenuItem(grapes);
         testMenu.addMenuItem(cassadilla);
         testMenu.addMenuItem(apple);
         testMenu.addMenuItem(yogurt);
+        testMenu.addMenuItem(apple2);
 
         testMenu.printFullMenu();
         testMenu.getMenuItem(grapes);
